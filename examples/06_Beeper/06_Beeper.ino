fee9c1e5cfae0359d1a8f6e330bed0401de79c30
Triqadafi <triqadafi@gmail.com>
2023-04-01 10:07:23 +0700

    add beeper example

---
+++ examples/06_Beeper/06_Beeper.ino
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+/*  
+ * EXAMPLE: Double beep every second
+ *
+ * API
+ * WatchOS.LED_write
+ * 
+ */
+/* USER DEFINE */
+/* USER DEFINE */
+
+#define DEBUG_F_ENABLE false
+
+
+
+/* END USER DEFINE*/
+
+#include "TQDF_WatchOS.h"
+#include "config_AEONF_mk6.h"
+
+/* USER VARIABLE and PROTOTYPES */
+
+
+
+
+/* END USER VARIABLE and PROTOTYPES*/
+
+void setup() {
+  WatchOS.config(
+    /* Serial Baudrate:  */ 115200, // 9600 or 115200
+    /* RTC Preserved     */ true, 
+    /* RTC Callibration: */ REPLACE_WITH_YOUR_CALIBRATION_VALUE, // From manufacturer (back of the PCB) fill with 0x0 to disable calibration value
+    /* WATCHDOG Routine  */ WatchOS.WAKE_DISABLED,
+    /* GPIO -------------------------------------------------------------------*/ 
+    /* Clock pin (1-12): */ led_clock_pins, 
+    /* LED Power pin:    */ led_power_pin, 
+    /* Button pin:       */ button_pin, 
+    /* Beeper pin:       */ beeper_pin, 
+    /* Unused pins:      */ unused_pins
+  );
+  /* USER SETUP CODE 1*/
+  // put your setup code here, to run once:
+
+
+
+  /* END USER SETUP CODE 1*/
+}
+
+/* USER APP CODE 1*/
+void loop() {
+  // WATCHDOG: 10 second of inactivity will shutdown the device!!!
+  // uncomment the program below if you dont want catched by the WATCHDOG
+  WatchOS.LED_write(NUMBER_F(12), LED_ON); // just tell the whatchdog that we are aware
+  
+  WatchOS.BEEPER_play(12500, 50); delay(50);
+  delay(50);
+  WatchOS.BEEPER_play(12500, 50); delay(50);
+  delay(50);
+  delay(800);
+
+}
+/* END USER APP CODE 1*/
